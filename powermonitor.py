1aecc957b69d479750a4a3e1336106e776a87b53
Markus Knapp <github@direcs.de>
2017-02-25 22:01:17 +0100

    Added callback for key press event. Changed time to 4 secs.

--- powermonitor.py
+++ powermonitor.py
@@ -8,9 +8,6 @@ except RuntimeError:
 # import time # for sleep
 import time # for sleep
 
-# timings
-secs = 2
-
 # init
 GPIO.setmode(GPIO.BCM) # use the GPIO names, _not_ the pin numbers on the board
 
@@ -19,24 +16,35 @@ ledPin     = 18 # pin 12
 switchPin  = 23 # pin 16
 batteryPin = 24 # pin 18
 
-# setup GPIO outputs
+# setup
 print('setup...')
 GPIO.setup(ledPin,   GPIO.OUT)
 GPIO.setup(switchPin, GPIO.IN)
 GPIO.setup(batteryPin, GPIO.IN)
 
+# setup callback for edge detection
+GPIO.add_event_detect(switchPin, GPIO.RISING)
+def my_callback():
+    print 'PUSHED!'
+GPIO.add_event_callback(switchPin, my_callback)
+
+# timings
+secs = 4
+
 #
 # loop
 #
+print('loop...')
 
 # turn LED on
 GPIO.output(ledPin, GPIO.LOW)
 
-# delay x seconds
+# delay
 time.sleep(secs)
 
 # turn LED off
 GPIO.output(ledPin, GPIO.HIGH)
+
 
 # cleanup
 GPIO.cleanup()

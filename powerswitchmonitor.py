d021d3d1c3db8fcb98c1a2419fb6b6afdb4bb000
Markus Knapp <github@direcs.de>
2017-02-25 20:46:23 +0100

    Added first LED test.

---
+++ powerswitchmonitor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+# import time # for sleep
+import time # for sleep
+
+# timings
+secs = 2
+
+# init
+GPIO.setmode(GPIO.BCM) # use the GPIO names, _not_ the pin numbers on the board
+
+# pins		BCM	  BOARD
+ledPin     = 18 # pin 12
+switchPin  = 23 # pin 16
+batteryPin = 24 # pin 18
+
+# setup GPIO outputs
+print('setup...')
+GPIO.setup(ledPin,   GPIO.OUT)
+GPIO.setup(switchPin, GPIO.IN)
+GPIO.setup(batteryPin, GPIO.IN)
+
+#
+# loop
+#
+
+# turn LED on
+GPIO.output(ledPin, GPIO.HIGH)
+
+# delay x seconds
+time.sleep(secs)
+
+# turn LED off
+GPIO.output(ledPin, GPIO.LOW)
+
+# cleanup
+GPIO.cleanup()

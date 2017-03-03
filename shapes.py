59586a8cfc5180b4c7429cf28d215fe4534685c6
Markus Knapp <github@direcs.de>
2017-03-03 21:33:07 +0000

    removed comments, removed shape drawing, added text for LCD

--- shapes.py
+++ shapes.py
@@ -12,41 +12,9 @@ from PIL import ImageFont
 
 # Raspberry Pi pin configuration:
 RST = 24
-# Note the following are only used with SPI:
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
-
-# Beaglebone Black pin configuration:
-# RST = 'P9_12'
-# Note the following are only used with SPI:
-# DC = 'P9_15'
-# SPI_PORT = 1
-# SPI_DEVICE = 0
 
 # 128x32 display with hardware I2C:
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-
-# 128x64 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
-
-# Note you can change the I2C address by passing an i2c_address parameter like:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
-
-# Alternatively you can specify an explicit I2C bus number, for example
-# with the 128x32 display you would use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
-
-# 128x32 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# 128x64 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# Alternatively you can specify a software SPI implementation by providing
-# digital GPIO pin numbers for all the required display pins.  For example
-# on a Raspberry Pi with the 128x32 display you might use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
 
 # Initialize library.
 disp.begin()
@@ -67,28 +35,6 @@ draw = ImageDraw.Draw(image)
 # Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = 2
-shape_width = 20
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = padding
-# Draw an ellipse.
-#draw.ellipse((x, top , x+shape_width, bottom), outline=255, fill=0)
-#x += shape_width+padding
-# Draw a rectangle.
-#draw.rectangle((x, top, x+shape_width, bottom), outline=255, fill=0)
-#x += shape_width+padding
-# Draw a triangle.
-#draw.polygon([(x, bottom), (x+shape_width/2, top), (x+shape_width, bottom)], outline=255, fill=0)
-#x += shape_width+padding
-# Draw an X.
-#draw.line((x, bottom, x+shape_width, top), fill=255)
-#draw.line((x, top, x+shape_width, bottom), fill=255)
-#x += shape_width+padding
-
 # Load default font.
 # font = ImageFont.load_default()
 
@@ -97,8 +43,8 @@ x = padding
 font = ImageFont.truetype('Roboto-Regular.ttf', 12)
 
 # Write two lines of text.
-draw.text((0, 0),  'Domo Arigato,',  font=font, fill=255)
-draw.text((0, 20), 'Mr. Roboto!', font=font, fill=255)
+draw.text((0, 0),  'Domo Arigato Roboto!',  font=font, fill=255)
+draw.text((0, 20), 'Battery: 11.1V', font=font, fill=255)
 
 # Display image.
 disp.image(image)

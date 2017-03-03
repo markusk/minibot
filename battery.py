1dc5a830652f422ab6744c36583300a888264e6b
Markus Knapp <github@direcs.de>
2017-03-03 22:15:28 +0000

    initial commit for AD conversion for battery check

---
+++ battery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+from MCP3008 import MCP3008
+
+adc = MCP3008()
+value = adc.read( channel = 0 ) # use channel 0 for battery voltage
+print("Voltage: %.2f" % (value / 1023.0 * 3.3) )

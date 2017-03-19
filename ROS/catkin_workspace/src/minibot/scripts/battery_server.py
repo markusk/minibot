4c51421702835b94fb79d4c1a6e11c96312468c3
Markus Knapp <github@direcs.de>
2017-03-20 00:19:53 +0100

    added comments

--- ROS/catkin_workspace/src/minibot/scripts/battery_server.py
+++ ROS/catkin_workspace/src/minibot/scripts/battery_server.py
@@ -3,13 +3,17 @@
 from minibot.srv import *
 import rospy
 
+# handle_add_two_ints is called with instances of AddTwoIntsRequest and returns instances of AddTwoIntsResponse
 def handle_add_two_ints(req):
     print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
     return AddTwoIntsResponse(req.a + req.b)
 
 def add_two_ints_server():
     rospy.init_node('battery_server')
+
+    # This declares a new service named battery with the Battery service type.
     s = rospy.Service('battery', Battery, handle_add_two_ints)
+
     print "Ready to provide battery details."
     rospy.spin()
 

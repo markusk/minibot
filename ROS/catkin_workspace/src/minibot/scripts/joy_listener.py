c42ed75f793f0a7189c4cfc42046b96f832aab14
Markus Knapp <github@direcs.de>
2017-04-06 09:32:10 +0200

    fixed message type, checking for button 1 only now. Works fine.

--- ROS/catkin_workspace/src/minibot/scripts/joy_listener.py
+++ ROS/catkin_workspace/src/minibot/scripts/joy_listener.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-## Simple talker demo that listens to std_msgs/Strings published 
-## to the 'joy' topic
+
+""" This is a simple listener for the joy_node. It listens on the topic 'joy' and prints out some information.
+
+Usage:
+1. Start roscore
+2. Set joystick device if different to js0: rosparam set joy_node/dev "/dev/input/js2"
+3. Run the joystick node: rosrun joy joy_node
+4. Run this listener: rosrun minibot joy_listener
+5. Press button 1 on th joystick and see the output. En-Joy! ;-)
+
+"""
 
 import rospy
-from std_msgs.msg import String
+from sensor_msgs.msg import Joy
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+def callback(joy):
+    # simple:
+    if (joy.buttons[0] == 1):
+      rospy.loginfo('Button 1 pressed!')
+
 
 def listener():
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
+    # name for our 'joy_listener' node so that multiple listeners can
     # run simultaneously.
     rospy.init_node('joy_listener', anonymous=True)
 
     # subscribe the joy(stick) topic
-    rospy.Subscriber('joy', String, callback)
+    rospy.Subscriber('joy', Joy, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()

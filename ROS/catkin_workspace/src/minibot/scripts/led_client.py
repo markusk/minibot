be2ad58904ea5dd6d4821a8072a31d75b2755df4
Markus Knapp <github@direcs.de>
2017-04-06 23:24:18 +0200

    Added tons of comments. Removed storing pin state.

--- ROS/catkin_workspace/src/minibot/scripts/led_client.py
+++ ROS/catkin_workspace/src/minibot/scripts/led_client.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# coding=utf-8
+
+# This is a client node
 
 import sys
 import rospy
@@ -6,9 +9,13 @@ import rospy
 from minibot.srv import *
 
 def led_switcher_client(pin):
+    # Service 'led' from led_server.py ready?
     rospy.wait_for_service('led')
     try:
+        # Create the handle 'led_switcher' with the service type 'Led'.
+        # The latter automatically generates the LedRequest and LedResponse objects.
         led_switcher = rospy.ServiceProxy('led', Led)
+        # the handle can be called like a normal function
         response = led_switcher(pin, state)
         return response.result
     except rospy.ServiceException, e:
@@ -26,5 +33,6 @@ if __name__ == "__main__":
         print usage()
         sys.exit(1)
 
-    # debug
+    # call the led_switcher function
+    # and show the result
     print "GPIO %s switched to %s. Result: %s"%(pin, state, led_switcher_client(pin))
--- ROS/catkin_workspace/src/minibot/scripts/led_server.py
+++ ROS/catkin_workspace/src/minibot/scripts/led_server.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+# This is a service node (server)
+
 # name of the package(!).srv
 from minibot.srv import *
 import rospy
 
-# for GPIO pin usage
+# for GPIO pin usage on the Raspberry Pi
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
     print("Error importing RPi.GPIO!")
 
-##
+
 ## GPIO stuff
-##
-GPIO.setmode(GPIO.BCM) # use the GPIO names, _not_ the pin numbers on the board
+# We use the GPIO names, _not_ the pin numbers on the board
+GPIO.setmode(GPIO.BCM)
 # Raspberry Pi pin configuration:
 # pins	    BCM   BOARD
 ledPin    = 18 # pin 12
@@ -29,15 +31,11 @@ pinListHigh = (ledPin)
 print "GPIO setup..."
 GPIO.setup(ledPin, GPIO.OUT)
 
-# all LOW
+# all pins which should be LOW at ini
 # GPIO.output(pinListLow, GPIO.LOW)
 
-# all HIGH
+# all pins which should be HIGH at ini
 GPIO.output(pinListHigh, GPIO.HIGH)
-
-# a list for saving the state of each of the 40 GPIO when changed via this node
-# False means LOW
-gpioStatus = [False for i in range(40)]
 
 
 # define a clean node exit
@@ -47,24 +45,22 @@ def my_exit():
   GPIO.cleanup()
   print "Done."
 
-# call this method on mode exit
+# call this method on node exit
 rospy.on_shutdown(my_exit)
 
 
-# handle_add_two_ints is called with instances of BatteryRequest and returns instances of BatteryResponse
+# handle_led is called with instances of LedRequest and returns instances of LedResponse
 # The request name comes directly from the .srv filename
 def handle_led(req):
-    # here is all the work done :)
+    """ In this function all the work is done :) """
 
-    # switch GPIO
+    # switch GPIO to HIGH, if '1' was sent
     if (req.state == 1):
       GPIO.output(req.pin, GPIO.HIGH)
     else:
+      # for all other values we set it to LOW
       # (LEDs are low active!)
       GPIO.output(req.pin, GPIO.LOW)
-
-    # store state in list
-    gpioStatus[req.pin-1] = False
 
     # debug
     print "GPIO %s switched to %s. Result: %s"%(req.pin, req.state, req.pin)
@@ -74,12 +70,16 @@ def handle_led(req):
 
 
 def led_server():
+    # Service nodes have to be initialised
     rospy.init_node('led_server')
 
-    # This declares a new service named battery with the Battery service type.
+    # This declares a new service named 'led'' with the 'Led' service type.
+    # All requests are passed to the 'handle_led' function.
+    # 'handle_led' is called with instances of LedRequest and returns instances of LedResponse
     s = rospy.Service('led', Led, handle_led)
-
     print "Ready to switch LEDs."
+
+    # Keep our code from exiting until this service node is shutdown
     rospy.spin()
 
 

114bc818eb4a2d7aa5ec2b230d76740e2648e1fc
Markus Knapp <github@direcs.de>
2017-04-09 00:19:03 +0200

    Added initial motor control code. Work in progress

--- ROS/catkin_workspace/src/minibot/scripts/motor_server.py
+++ ROS/catkin_workspace/src/minibot/scripts/motor_server.py
@@ -7,6 +7,13 @@
 from minibot.srv import *
 import rospy
 
+# for getting the hostname of the underlying system
+#import socket
+# showing hostname
+#hostname = socket.gethostname()
+#print("Running on host " + hostname + ".")
+
+
 # for GPIO pin usage on the Raspberry Pi
 try:
     import RPi.GPIO as GPIO
@@ -38,9 +45,38 @@ GPIO.setup(ledPin, GPIO.OUT)
 GPIO.output(pinListHigh, GPIO.HIGH)
 
 
+###### motor stuff
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+
+# run some parts only on the real robot
+# if hostname == 'minibot':
+# create a default motor object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT(addr=0x60)
+
+# using motor 1 and 2 on RasPi hat
+myMotor1 = mh.getMotor(1)
+myMotor2 = mh.getMotor(2)
+
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+    myMotor1.run(Adafruit_MotorHAT.RELEASE);
+    myMotor2.run(Adafruit_MotorHAT.RELEASE);
+
+# turning off motors NOW
+turnOffMotors();
+
+# set the speed (from 0 (off) to 255 (max speed))
+startSpeed = 100
+myMotor1.setSpeed(startSpeed)
+myMotor2.setSpeed(startSpeed)
+
+
 # define a clean node exit
 def my_exit():
-  print "Shutting LED server down..."
+  rospy.loginfo('Shutting down motor service...')
+  # run some parts only on the real robot
+  # if hostname == 'minibot':
+  turnOffMotors();
   # GPIO cleanup
   GPIO.cleanup()
   print "Done."
@@ -77,7 +113,7 @@ def led_server():
     # All requests are passed to the 'handle_led' function.
     # 'handle_led' is called with instances of LedRequest and returns instances of LedResponse
     s = rospy.Service('led', Led, handle_led)
-    print "Ready to switch LEDs."
+    rospy.loginfo('Ready to switch motors.')
 
     # Keep our code from exiting until this service node is shutdown
     rospy.spin()
@@ -85,3 +121,47 @@ def led_server():
 
 if __name__ == "__main__":
     led_server()
+
+
+
+""" motor stuff to be implemented here:
+    ### drive
+    # run some parts only on the real robot
+    if hostname == 'minibot':
+        # drive
+        print("Forward! ")
+        myMotor1.run(Adafruit_MotorHAT.FORWARD)
+        myMotor2.run(Adafruit_MotorHAT.FORWARD)
+
+        print("\tSpeed up...")
+        for i in range(startSpeed, 255):
+            myMotor1.setSpeed(i)
+            myMotor2.setSpeed(i)
+            time.sleep(0.01)
+
+        print("\tSlow down...")
+        for i in range(255, startSpeed, -1):
+            myMotor1.setSpeed(i)
+            myMotor2.setSpeed(i)
+            time.sleep(0.01)
+
+        print("Backward! ")
+        myMotor1.run(Adafruit_MotorHAT.BACKWARD)
+        myMotor2.run(Adafruit_MotorHAT.BACKWARD)
+
+        print("\tSpeed up...")
+        for i in range(startSpeed, 255):
+            myMotor1.setSpeed(i)
+            myMotor2.setSpeed(i)
+            time.sleep(0.01)
+
+        print("\tSlow down...")
+        for i in range(255, startSpeed, -1):
+            myMotor1.setSpeed(i)
+            myMotor2.setSpeed(i)
+            time.sleep(0.01)
+
+        print("Release")
+        myMotor1.run(Adafruit_MotorHAT.RELEASE)
+        myMotor2.run(Adafruit_MotorHAT.RELEASE)
+"""

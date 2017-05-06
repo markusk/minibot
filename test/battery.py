4cbb7fb7fb213d97c859630621ff80e9520bbe51
Markus Knapp <github@direcs.de>
2017-05-06 21:31:46 +0200

    Now displaying battery voltage

--- test/battery.py
+++ test/battery.py
@@ -4,5 +4,12 @@
 from MCP3008 import MCP3008
 
 adc = MCP3008()
-value = adc.read( channel = 0 ) # use channel 0 for battery voltage
-print("Voltage: %.2f" % (value / 1023.0 * 3.3) )
+voltage = 0
+
+# read AD converter (battery voltage)
+# use channel 0 on IC
+value = adc.read(channel = 0)
+# 2.73 V = 12.32 V (measured) > 1023 / 3.3 * 2.73 / 12.32 = 68.693182
+voltage = (value / 68.693182)
+#print("Value: %d" % value)
+print("Battery: %.1f Volt" % voltage)

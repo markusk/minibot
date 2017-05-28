b0fd770641131ba0fbe8de663013abd6a9043fc6
Markus Knapp <github@direcs.de>
2017-05-28 20:40:11 +0200

    Initial import. Will become a motor driver later.

---
+++ ROS/catkin_workspace/src/minibot/scripts/keyboard_motor_listener.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+## Simple keyboard_listener demo
+## that listens to geometry_msgs/Twist published by the 'cmd_vel' topic
+## of the teleop_twist_keyboard node
+
+import rospy
+from geometry_msgs.msg import Twist
+
+def callback(data):
+    # print out received message from the teleop_twist_keyboard
+    rospy.loginfo(rospy.get_caller_id() + ' received x=%s', data.linear.x)
+    rospy.loginfo(rospy.get_caller_id() + ' received z=%s', data.angular.z)
+
+def listener():
+
+    # In ROS, nodes are uniquely named. The anonymous=True flag
+    # means that rospy will choose a unique name for this listener node
+    rospy.init_node('keyboard_listener', anonymous=True)
+
+    # subscribe the message cmd_vel
+    rospy.Subscriber('cmd_vel', Twist, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()

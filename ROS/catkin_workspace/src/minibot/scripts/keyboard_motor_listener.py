55321be75882188e375eec42c476dd800d093977
Markus Knapp <github@direcs.de>
2017-05-28 21:09:52 +0200

    Added a drive function to control a motor (via node) and some callback data checks to call the drive function.

--- ROS/catkin_workspace/src/minibot/scripts/keyboard_motor_listener.py
+++ ROS/catkin_workspace/src/minibot/scripts/keyboard_motor_listener.py
@@ -7,10 +7,62 @@
 import rospy
 from geometry_msgs.msg import Twist
 
+# The minibot package has the motor server, which listens to messages from here
+from minibot.srv import *
+
+# set the motor speed (from 0 (off) to 255 (max speed))
+startSpeed = 100
+
+
+#  this will execute the "drive" command
+def drive(direction):
+    # Service 'motor' from motor_server.py ready?
+    rospy.wait_for_service('motor')
+
+    # Send driving direction to motor
+    try:
+        # Create the handle 'motor_switcher' with the service type 'Motor'.
+        # The latter automatically generates the MotorRequest and MotorResponse objects.
+        motor_switcher = rospy.ServiceProxy('motor', Motor)
+
+         # the handle can be called like a normal function
+        rospy.loginfo("Switching motors to %s @ speed %s.", direction, startSpeed)
+        response = motor_switcher(direction, startSpeed)
+
+        # show result
+        rospy.loginfo(rospy.get_caller_id() + ' says result is %s.', response.result)
+
+    except rospy.ServiceException, e:
+        rospy.logerr("Service call for 'motor' failed: %s", e)
+
+
 def callback(data):
     # print out received message from the teleop_twist_keyboard
     rospy.loginfo(rospy.get_caller_id() + ' received x=%s', data.linear.x)
     rospy.loginfo(rospy.get_caller_id() + ' received z=%s', data.angular.z)
+
+    # which key / Speed was pressed?
+    #
+    if  (data.linear.x == 1.0):
+      rospy.loginfo("FORWARD key pressed.")
+      drive("FORWARD")
+    #
+    elif (data.linear.x == 2.0):
+      rospy.loginfo("BACKWARD key pressed.")
+      drive("BACKWARD")
+    #
+    elif (data.linear.x == 3.0):
+      rospy.loginfo("LEFT key pressed.")
+      drive("LEFT")
+    #
+    elif (data.linear.x == 4.0):
+      rospy.loginfo("RIGHT key pressed.")
+      drive("RIGHT")
+    #
+    elif (data.linear.x == 5.0):
+      rospy.loginfo("STOP key pressed.")
+      drive("STOP")
+
 
 def listener():
 

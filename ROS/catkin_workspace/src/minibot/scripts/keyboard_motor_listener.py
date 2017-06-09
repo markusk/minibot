7006ffd484c3dd66bedbf355f31854ed8fecfb3d
Markus Knapp <github@direcs.de>
2017-06-09 23:42:11 +0200

    Disabled debug lines

--- ROS/catkin_workspace/src/minibot/scripts/keyboard_motor_listener.py
+++ ROS/catkin_workspace/src/minibot/scripts/keyboard_motor_listener.py
@@ -41,8 +41,8 @@ def drive(direction):
 
 def callback(data):
     # print out received message from the teleop_twist_keyboard
-    rospy.loginfo(rospy.get_caller_id() + ' received x=%s', data.linear.x)
-    rospy.loginfo(rospy.get_caller_id() + ' received z=%s', data.angular.z)
+    # rospy.loginfo(rospy.get_caller_id() + ' received x=%s', data.linear.x)
+    # rospy.loginfo(rospy.get_caller_id() + ' received z=%s', data.angular.z)
 
     # which key / Speed was pressed?
     # i key

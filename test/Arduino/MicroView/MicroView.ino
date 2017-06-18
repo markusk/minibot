227bd5ca889adc8ed2b3cc51746ac776248d9220
Markus Knapp <github@direcs.de>
2017-06-18 11:45:34 +0200

    added first Adafruit BNO055 code, removed forgotten unneed old code.

--- test/Arduino/MicroView/MicroView.ino
+++ test/Arduino/MicroView/MicroView.ino
@@ -1,22 +1,28 @@
+// MicroView OLED
 #include <MicroView.h>
-#include <Time.h>
 
-#define clocksize 24
+// Adafruit BNO055
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
-uint16_t 	onDelay=5;		// this is the on delay in milliseconds, if there is no on delay, the erase will be too fast to clean up the screen.
 
-
-void setup() {
-	uView.begin();		// begin of MicroView
+void setup()
+{
+        // begin of MicroView
+	uView.begin();		
 	uView.clear(ALL);	// erase hardware memory inside the OLED controller
 	uView.display();	// display the content in the buffer memory, by default it is the MicroView logo
-	setTime(10,10,01,17,1,2014);
 	delay(700);
 	uView.clear(PAGE);	// erase the memory buffer, when next uView.display() is called, the OLED will be cleared.
+
 }
 
 
-void loop() {
+void loop()
+{
+        // MicroView
         // TEXT Font 0
 	uView.clear(PAGE);
 	uView.setCursor(0,40);

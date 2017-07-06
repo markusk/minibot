d0652706cc036ba51b7cc90dd6f60e15fa8a6a29
Markus Knapp <github@direcs.de>
2017-07-06 18:43:22 +0200

    Converting...

--- ROS/catkin_workspace/src/minibot/scripts/imu_bno055.py
+++ ROS/catkin_workspace/src/minibot/scripts/imu_bno055.py
@@ -2,32 +2,31 @@
 # coding=utf-8
 
 #include<ros/ros.h>
+import rospy
 #include<geometry_msgs/Twist.h>
+from geometry_msgs.msg import Twist
+
 #include<sensor_msgs/Imu.h>
+from sensr_msgs import Imu
+
 #include<iostream>
 #include<tf/LinearMath/Matrix3x3.h>
 #include<tf/LinearMath/Quaternion.h>
 
-using namespace std;
+#using namespace std;
 
-class TeleopImu{
-public:
-  TeleopImu();
-private:
-  void callBack(const sensor_msgs::Imu::ConstPtr& imu);
-  ros::NodeHandle n;
-  ros::Publisher pub;
-  ros::Subscriber sub;
-};
+# c variables
+ros::NodeHandle n;
+ros::Publisher pub;
+ros::Subscriber sub;
 
-TeleopImu::TeleopImu()
-{
+
+def TeleopImu():
 	pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel",1);
 	sub = n.subscribe<sensor_msgs::Imu>("imu/data", 10, &TeleopImu::callBack, this);
-}
 
-void TeleopImu::callBack(const sensor_msgs::Imu::ConstPtr& imu)
-{
+
+def callBack(const sensor_msgs::Imu::ConstPtr& imu):
 	geometry_msgs::Twist vel;
 	tf::Quaternion bq(imu->orientation.x,imu->orientation.y,imu->orientation.z,imu->orientation.w);
 	double roll,pitch,yaw;
@@ -35,11 +34,22 @@ void TeleopImu::callBack(const sensor_msgs::Imu::ConstPtr& imu)
 	vel.angular.z = roll;
 	vel.linear.x = pitch;
 	pub.publish(vel);
-}
 
-int main(int argc, char** argv)
-{
-	ros::init(argc, argv, "teleopImu");
+
+def listener():
+
+    # In ROS, nodes are uniquely named. The anonymous=True flag
+    # means that rospy will choose a unique name for this listener node
+    rospy.init_node('teleopImu', anonymous=True)
+
+    # Ready
+    rospy.loginfo("Ready. Start Turtlesim and move sensor around to move the turtle.")
+
 	TeleopImu teleop_turtle;
-	ros::spin();
-}
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()

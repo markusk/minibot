97d2198fb48c59c8d79f863e2da3d8feeb64b5d7
Markus Knapp <github@direcs.de>
2017-07-12 21:37:48 +0200

    Changed print to rospy.loginfo/err

--- ROS/catkin_workspace/src/minibot/scripts/Bno055_node.py
+++ ROS/catkin_workspace/src/minibot/scripts/Bno055_node.py
@@ -48,34 +48,41 @@ bno = BNO055.BNO055()
 
 # Initialize the BNO055 and stop if something went wrong.
 if not bno.begin():
-    raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+    rospy.logerr("Failed to initialize BNO055! Is the sensor connected?")
 
 # Print system status and self test result.
 status, self_test, error = bno.get_system_status()
-print('System status: {0}'.format(status))
-print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
+rospy.loginfo('system status is {} {} {} '.format(status))
+rospy.loginfo('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
+
 # Print out an error if system status is in error mode.
 if status == 0x01:
-    print('System error: {0}'.format(error))
-    print('See datasheet section 4.3.59 for the meaning.')
+    rospy.logerr('System error: {0}'.format(error))
+    rospy.loginfo('See datasheet section 4.3.59 for the meaning.')
 
 # Print BNO055 software revision and other diagnostic data.
 sw, bl, accel, mag, gyro = bno.get_revision()
-print('Software version:   {0}'.format(sw))
-print('Bootloader version: {0}'.format(bl))
-print('Accelerometer ID:   0x{0:02X}'.format(accel))
-print('Magnetometer ID:    0x{0:02X}'.format(mag))
-print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
-
-print('Reading BNO055 data, press Ctrl-C to quit...')
 while True:
+rospy.loginfo('Software version:   {0}'.format(sw))
+rospy.loginfo('Bootloader version: {0}'.format(bl))
+rospy.loginfo('Accelerometer ID:   0x{0:02X}'.format(accel))
+rospy.loginfo('Magnetometer ID:    0x{0:02X}'.format(mag))
+rospy.loginfo('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
+
+
+rospy.loginfo('Reading BNO055 data, press Ctrl-C to quit...')
+
+
     # Read the Euler angles for heading, roll, pitch (all in degrees).
     heading, roll, pitch = bno.read_euler()
+
     # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
     sys, gyro, accel, mag = bno.get_calibration_status()
+
     # Print everything out.
-    print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
+    rospy.loginfo('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
           heading, roll, pitch, sys, gyro, accel, mag))
+
     # Other values you can optionally read:
     # Orientation as a quaternion:
     #x,y,z,w = bno.read_quaterion()
@@ -93,5 +100,6 @@ while True:
     # Gravity acceleration data (i.e. acceleration just from gravity--returned
     # in meters per second squared):
     #x,y,z = bno.read_gravity()
+
     # Sleep for a second until the next reading.
     time.sleep(1)

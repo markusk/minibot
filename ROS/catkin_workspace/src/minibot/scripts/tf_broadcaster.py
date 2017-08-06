fe19e052911a272e2efd0a5d1fd80288a5e7073f
Markus Knapp <github@direcs.de>
2017-08-06 23:10:27 +0200

    Added code for the transform tree setup and broadcasting. To be tested.

--- ROS/catkin_workspace/src/minibot/scripts/tf_broadcaster.py
+++ ROS/catkin_workspace/src/minibot/scripts/tf_broadcaster.py
@@ -2,6 +2,29 @@
 # coding=utf-8
 
 """
-This node does the transform tree setup for my minibot.
-It also publishes the transforms into ROS.
+This node does the transform tree setup for my robot 'minibot'.
+It also publishes these transforms into ROS.
 """
+
+import logging
+import rospy
+import tf
+
+if __name__ == '__main__':
+    # init node
+    rospy.init_node('robot_tf_broadcaster')
+    rospy.loginfo("robot_tf_broadcaster started.")
+
+    # the broacaster (publisher)
+    br = tf.TransformBroadcaster()
+    rate = rospy.Rate(10.0)
+
+    while not rospy.is_shutdown():
+        # send!
+        br.sendTransform((0.0, 0.0, 0.0, 1.0),
+                         (0.5, 0.1, 0.0), # @todo: check real measurements!
+                         rospy.Time.now(),
+                         "bae_link",
+                         "laser_link")
+                         # @todo: what about IMU here?!?
+        rate.sleep()

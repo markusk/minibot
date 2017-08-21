71b2bfd76cb3ce231f6c979b9e226931286d86a5
Markus Knapp <github@direcs.de>
2017-08-21 23:38:38 +0200

    added hostname check and simulation value

--- ROS/catkin_workspace/src/minibot/nodes/battery_publisher.py
+++ ROS/catkin_workspace/src/minibot/nodes/battery_publisher.py
@@ -25,11 +25,25 @@ pub = rospy.Publisher('voltage', Float32, queue_size=10)
 # Begin loop @ one Hz
 rate = rospy.Rate(1)
 
+
+#
+# for getting the hostname of the underlying system
+import socket
+# showing hostname
+hostname = socket.gethostname()
+rospy.loginfo("Running on host %s.", hostname)
+if hostname != 'minibot':
+    rospy.logwarn("Test mode only due to other host. Skipping all SPI staff!")
+
+
 #
 # for the AD converter (connected to Raspberry Pi via SPI)
 #
-from MCP3008 import MCP3008
-adc = MCP3008()
+# run some parts only on the real robot
+if hostname == 'minibot':
+    from MCP3008 import MCP3008
+    adc = MCP3008()
+
 voltage = 0.0
 value = 0
 
@@ -37,9 +51,15 @@ value = 0
 while not rospy.is_shutdown():
     pub.publish(voltage)
 
-    # read AD converter (battery voltage)
-    # use channel 0 on IC
-    value = adc.read(channel = 0)
+    # run some parts only on the real robot
+    if hostname == 'minibot':
+        # read AD converter (battery voltage)
+        # use channel 0 on IC
+        value = adc.read(channel = 0)
+    else:
+        # simulated value!
+        value = 0
+
     # 2.73 V = 12.32 V (measured) > 1023 / 3.3 * 2.73 / 12.32 = 68.693182
     voltage = (value / 68.693182)
     # Print out voltage

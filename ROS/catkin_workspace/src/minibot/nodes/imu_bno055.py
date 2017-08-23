88995f86d3db6e4ebe662354e187f17552a06dc4
Markus Knapp <github@direcs.de>
2017-08-23 23:27:36 +0200

    Removed old comment

--- ROS/catkin_workspace/src/minibot/nodes/imu_bno055.py
+++ ROS/catkin_workspace/src/minibot/nodes/imu_bno055.py
@@ -145,9 +145,6 @@ else:
 
 
 while not rospy.is_shutdown():
-    # check for incoming messages
-    # rospy.spinOnce() >> this does not exist in rospy. Unneeded since we do not receive messages? Otherwise create a listener.
-
     # for header time stamps
     current_time = rospy.Time.now()
 

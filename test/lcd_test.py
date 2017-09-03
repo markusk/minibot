024235361214b7d5f704edb12d50c174dfb612c2
Markus Knapp <github@direcs.de>
2017-09-03 14:15:33 +0200

    Added font awesome battery symbol

--- test/lcd_test.py
+++ test/lcd_test.py
@@ -2,14 +2,9 @@
 # coding=utf-8
 
 import time
-
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
-
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
+from PIL import Image, ImageDraw, ImageFont
 
 # Raspberry Pi pin configuration:
 RST = 24
@@ -24,11 +19,23 @@ disp.begin()
 disp.clear()
 disp.display()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
+# Create blank image for drawing. Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
+
+# The fonts and sizes
+size = 15
+symbolWidth = 25
+# text
+font1 = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', size)
+# https://fontawesome.io
+# install via sudo 'sudo apt install fonts-font-awesome'
+font2 = ImageFont.truetype('/usr/share/fonts/truetype/font-awesome/fontawesome-webfont.ttf', size)
+
+# see http://fontawesome.io/cheatsheet/
+# battery-full = '' + unichr(0xf240)
+# battery-full = unicode(u'0xf240')
 
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
@@ -36,17 +43,13 @@ draw = ImageDraw.Draw(image)
 # Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Load default font.
-# font = ImageFont.load_default()
-size = 15
-font1 = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', size)
-# install via sudo 'sudo apt install fonts-font-awesome'
-font2 = ImageFont.truetype('/usr/share/fonts/truetype/font-awesome/fontawesome-webfont.ttf', size)
-
-# Write two lines of text.
-draw.text((0, 0),    'Domo Arigato :)',  font=font1, fill=255)
-# draw.text((0, size), 'Battery: 99.9 V',  font=font2, fill=255)
-draw.text((0, size), unichr(0xf240),  font=font2, fill=255)
+# Write lines of text.
+# line 1
+draw.text((0, 0),              'Battery-Status:', font=font1, fill=255)
+# line 2, first symbol
+draw.text((0, size),           unichr(0xf240),    font=font2, fill=255)
+# line 2, text after symbol
+draw.text((symbolWidth, size), '12.0 Volt',       font=font1, fill=255)
 
 # Display image.
 disp.image(image)

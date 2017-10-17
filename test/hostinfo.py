2439125950e7de8aa9332ae0b329700ce36b6968
Markus Knapp <github@direcs.de>
2017-10-18 00:35:42 +0200

    Getting IPV4 adress now without knowing interface name

--- test/hostinfo.py
+++ test/hostinfo.py
@@ -4,17 +4,19 @@
 
 # for getting the hostname and IP of the underlying system
 import socket
-import fcntl
-import struct
+import subprocess
+
+""" import fcntl
+import struct """
 
 # get hostname
 hostname = socket.gethostname()
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # # 0x8915 is SIOCGIFADDR
-    return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', ifname[:15]))[20:24])
+# get IP
+ip = subprocess.check_output(['hostname', '-I'])
+# erstes leerzeichen finden und dort abschneiden
+ip4 = ip[:ip.index(" ")]
 
 # show it
 print("Running on host %s.", hostname)
-print("IP address of eth0 is %s.", get_ip_address('eth0'))
+print("IP4 address is %s.", ip4)

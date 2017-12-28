f205ad9cb8f4d3a29f65bfe50fceed0d9c6ea7e4
Markus Knapp <github@direcs.de>
2017-12-28 18:23:03 +0100

    Udpated ADC to Adafruit ADS1015

--- ROS/catkin_workspace/src/minibot/nodes/battery_publisher.py
+++ ROS/catkin_workspace/src/minibot/nodes/battery_publisher.py
@@ -60,8 +60,14 @@ seq = 0
 """ for the AD converter (connected to Raspberry Pi via SPI) """
 # run some parts only on the real robot
 if hostname == 'minibot':
-    from MCP3008 import MCP3008
-    adc = MCP3008()
+    import Adafruit_ADS1x15
+
+    # the Analog Digital converter object
+    adc = Adafruit_ADS1x15.ADS1015()
+
+    """ Gain 1 means, max a value of +4.096 Volt (+4,096 Volt in Europe) on the ADC channel, resulting in a 'value' of +2047. """
+    GAIN = 1
+    voltage = 0
 
 
 while not rospy.is_shutdown():
@@ -83,9 +89,12 @@ while not rospy.is_shutdown():
     if hostname == 'minibot':
         # read AD converter (battery voltage)
         # use channel 0 on IC
-        value = adc.read(channel = 0)
-        # 2.73 V = 12.32 V (measured) > 1023 / 3.3 * 2.73 / 12.32 = 68.693182
-        voltage = (value / 68.693182)
+        value = adc.read_adc(0, gain=GAIN)
+        # 13.435 Volt battery voltage resulted in 3.845(! wtf?) Volt on the ADC channel with my circuit (voltage divider).
+        # This resulted in a ADC 'value' of 1896.
+        # The conversion factor for the battery voltage is then: 1896 / 13.435 = 141.123930033494604
+        #
+        voltage = (value / 141.123930033494604)
     else:
         # simulated value!
         voltage = demoVoltage
@@ -118,7 +127,7 @@ while not rospy.is_shutdown():
     battery_msg.present = True
 
     # this is the battery voltage
-    # @TODO strange, this assignment as to be exacty here...
+    # @TODO strange, this assignment as to be exactly here...
     battery_msg.voltage = voltage
 
     # print out voltage message

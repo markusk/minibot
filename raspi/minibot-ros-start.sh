236ddec085f0988cb78ae95d958decaf4bfc6b9e
Markus Knapp <github@direcs.de>
2019-11-13 22:15:47 +0100

    Added a service file for systemd

---
+++ raspi/minibot-ros-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# needed for ROS
+source /opt/ros/kinetic/setup.bash
+
+# need to find the ROS workspace
+source /home/pi/catkin_ws/devel/setup.bash
+
+# change to ROS workspace (not really necessary)
+cd /home/pi/catkin_ws
+
+# start ROS
+roslaunch minibot minibot.launch
